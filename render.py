b16055ef2233337a0e6e40c912e592daa173aa51
Henrique <henriquemoreira10fk@gmail.com>
2023-11-30 11:49:04 -0300

    `fix` some ffmpeg arg errors

--- render.py
+++ render.py
@@ -8,10 +8,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description='MassVideoRenderer')
 parser.add_argument('-v',  '--version',                        action='version', version='MassVideoRenderer v1.0.0')
-parser.add_argument('-i',  '--input',                          metavar='input',            type=str, required=True, help='Input directory',)
-parser.add_argument('-o',  '--output',                         metavar='output',           type=str, required=True, help='Output directory')
+parser.add_argument('-i',  '--input',                          metavar='input',            type=str, required=True,  help='Input directory',)
+parser.add_argument('-o',  '--output',                         metavar='output',           type=str, required=True,  help='Output directory')
 parser.add_argument('-ie', '--input-extensions', default=None, metavar='input_extensions', type=str, required=False, help='Input files extensions (separated by comma)')
-parser.add_argument('-oe', '--output-extension',               metavar='output_extension', type=str, required=True, help='Output files extension')
+parser.add_argument('-oe', '--output-extension',               metavar='output_extension', type=str, required=True,  help='Output files extension')
 
 args = parser.parse_args()
 
@@ -35,25 +35,32 @@ class settings:
         ffmpeg_log_level_debug = 'warning'  # FFmpeg log level debug [str = 'warning']
 
     ffmpeg_render_args = {
-        'hwaccel': None,  # Hardware acceleration API [str = None]
-        'hwaccel_device': None,  # Hardware acceleration device (GPU) ID [int = None]
-        'c:v': 'libsvtav1',  # Video codec [str]
-        'minrate:v': '2m',  # Minimum video bit rate [str]
-        'maxrate:v': '10m',  # Maximum video bit rate [str]
-        'quality': 'high',  # Quality preset [str]
-        'b:v': 0,  # Video bit rate (0 = VBR) [int]
-        'tune': 'stillimage',  # Tune preset [str]
+        'hwaccel': None,  # Hardware acceleration API [str = None] -> 'cuda', 'd3d11va', 'opencl', ...
+        'hwaccel_device': None,  # Hardware acceleration device (GPU) ID [int = None] -> 0, 1, 2, ...
+        'c:v': 'libsvtav1',  # Video codec [str] -> 'libsvtav1', 'libx264', 'libx265', ...
+        'minrate:v': '2m',  # Minimum video bit rate [str] -> '2m', '4m', '8m', ...
+        'maxrate:v': '8m',  # Maximum video bit rate [str] -> '2m', '4m', '8m', ...
+        'quality': 'high',  # Quality preset [str] -> 'high', 'medium', 'low', ...
+        'level': 4.0,  # Level [float] -> 1.0, 2.0, 3.0, 4.0, 5.0, ...
+        'b:v': 0,  # Video bit rate [int] (0 = VBR)
+        'tune': 0,  # Tune preset [str]
         'framerate': '60',  # Frame rate [str]
         'threads': 0,  # Number of threads [int = 0]
-        'tile_columns': 2,  # Number of tile columns [int]
-        'tile_rows': 2,  # Number of tile rows [int]
-        'profile:v': 'main',  # Video profile [str]
-        'pred': None,  # Prediction mode [int]
-        'b_strategy': None,  # B-frames strategy [int]
+        'tile_columns': 2,  # Number of tile columns [int] -> tile_columns * tile_rows = YOUR_CPU_CORES
+        'tile_rows': 4,  # Number of tile rows [int] -> tile_rows * tile_columns = YOUR_CPU_CORES
+        'profile:v': 'main',  # Video profile [str] -> 'main', 'high', ...
+        'pred': 'complex',  # Prediction mode [int] -> 'simple', 'complex', ...
+        'b_strategy': 1,  # B-frames strategy [int] -> 0, 1, 2, 3, ...
+        'bf': 2,  # Number of B-frames [int] -> 1, 2, 3, ...
         'c:a': 'libopus',  # Audio codec [str]
-        'b:a': '128kbps',  # Audio bit rate [str]
+        'b:a': '128k',  # Audio bit rate [str]
         'ar': '48000',  # Audio sample rate [str]
         'c:s': 'webvtt',  # Subtitle codec [str]
+        'pix_fmt': 'yuv444p',  # Pixel format [str] -> 'yuv420p', 'yuv422p', 'yuv444p', ...
+        'sharpness': 0.5,  # Sharpness [float] -> 0.1, 0.2, 0.3, ...
+        'gamma': 2.2,  # Gamma [float] -> 0.1, 0.2, 0.3, ...
+        'deblock': 4,  # Deblocking [float] -> 0.1, 0.2, 0.3, ...
+        'noise_reduction': 0.5,  # Noise reduction [float] -> 0.1, 0.2, 0.3, ...
         'additional_metadata': {
             'metadata title=': None,  # Title [str]
             'metadata:s:v:0 title=': None,  # Title [str]
@@ -90,15 +97,21 @@ def flatten_dict(d, parent_key=str(), sep=':', skip_none=True) -> list:
 
 def render_file(input: pathlib.Path, output: pathlib.Path, now_ep_number: int, total_eps_quantity: int) -> int:
     # Creating FFmpeg arguments
+    output_dir = output.absolute()
     ffmpeg_other_args = [f'-{key} {value}' if value else f'-{key}' for key, value in flatten_dict(settings.ffmpeg_render_args, sep=' ')]
     ffmpeg_other_args.insert(0, f'-i "{input.absolute()}"')
-    ffmpeg_other_args.append(f'"{output.absolute()}"')
 
-    print(f'INPUT: {input.absolute()}')
-    print(f'OUTPUT: {output.absolute()}')
-    # Adding additional metadata
+    # Adding additional arguments
     if settings.ffmpeg_settings.replace_existing_file:
         ffmpeg_other_args.insert(1, '-y')
+        ffmpeg_other_args.append(f'"{output_dir}"')
+    else:
+        output_wo_ext = output_dir.parent / output_dir.stem
+        while output_dir.exists():
+            output_wo_ext = pathlib.Path(f'{output_wo_ext}_new')
+            output_dir = pathlib.Path(f'{output_wo_ext}.{settings.general.output_extension}')
+        ffmpeg_other_args.append(f'"{output_dir}"')
+
     if settings.ffmpeg_settings.hide_ffmpeg_banner:
         ffmpeg_other_args.insert(2, '-hide_banner')
     if settings.ffmpeg_settings.show_extra_ffmpeg_debug_info:
@@ -133,7 +146,6 @@ def main() -> None:
 
     for input_file in total_file_list:
         render_start_time = time.time()
-
         now_ep_number += 1
         print(f'\n[info] Queue: {now_ep_number}/{total_file_quantity} - Starting transcoding of file "{input_file}"...')
         output_file = pathlib.Path(settings.general.output, f'{input_file.stem}.{settings.general.output_extension}')
